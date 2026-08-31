cc278b7b4f6520a3334f7bb9c9f3aa459788f434
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 16:12:48 +0530

    ✅ OJJAR65 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR65/solution.cpp
@@ -0,0 +1,20 @@
+
+  }; 
+      textAlign: "center",
+      boxShadow: "2px 2px 10px rgba(0,0,0,0.1)"
+      borderRadius: "10px", 
+      width: "250px",
+      padding: "20px", 
+  const headingStyle = {
+      color: "blue",
+      fontSize: "20px"
+  };
+
+  return (  
+      <div >
+      </div>  
+  );  
+}
+
+        <GreetingCard card={cardStyle} head={headingStyle} name={name} age={age} greeting={greeting}></</GreetingCard>
+export default App;
