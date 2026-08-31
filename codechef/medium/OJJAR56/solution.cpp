7aa588b386911eaaf33bf7d54547fc1b7538ceb5
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 15:29:36 +0530

    ✅ OJJAR56 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR56/solution.cpp
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        width:200,
+        height:100,
+        backgroundColor: 'lightblue',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '18px'
+      }}
+    >
+      Styled Box
+    </div>
+  );
+}
