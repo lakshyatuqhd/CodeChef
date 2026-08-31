972a52f40e30c4bb92a1bac7ef83a4c0a012885b
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 15:40:35 +0530

    ✅ OJJAR62 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR62/solution.cpp
@@ -0,0 +1,24 @@
+function Welcome() {
+  return <h2>Welcome to My React App! 🎉</h2>;
+}
+
+// Creating the new Goodbye component
+function Goodbye(){
+
+
+
+function App() {
+  return (
+    <div>
+      <Welcome /> {/* Using the Welcome component */}
+      <p>React components make the UI more structured and reusable.</p>
+      {/* Using the Goodbye component */}
+    
+    
+    </div>
+  );
+}
+  return <h3>Thanks for visiting!See you soon!</h3>
+}
+
+export default App;
