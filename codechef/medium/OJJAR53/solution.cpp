ec505cf2761f216c0b50da12957e89f8bfcf1ed5
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 15:27:33 +0530

    ✅ OJJAR53 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR53/solution.cpp
@@ -0,0 +1,15 @@
+// update the function
+export function App() { 
+  const username = "";  
+  const currentYear = new Date().getFullYear();  
+  
+  return (
+    <div>
+
+Welcome, {username}! Happy {currentYear}!
+
+    </div>
+  );
+}
+
+export default App;
