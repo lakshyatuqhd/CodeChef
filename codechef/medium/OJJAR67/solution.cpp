a017c8b2fbde1f163fc09fbdc587abf9c54504a6
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 16:17:14 +0530

    ✅ OJJAR67 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR67/solution.cpp
@@ -0,0 +1,29 @@
+
+
+export default App;
+
+    </div>
+  );
+};
+
+
+
+     
+     />
+     avatarUrl={avatarUrl}
+     bio={bio}
+     <ProfileCard
+     name={name}
+      </h1>
+    <div>
+
+      <h1 style={{ textAlign: "center", fontFamily: "Arial, sans-serif" }}>
+        profile Card
+  const bio = "";
+
+  return (
+  const name = "";
+  // update the user name and bio
+
+  const avatarUrl = "https://cdn.codechef.com/images/problems/PREACT018/a29545c678c75e59bc684868407b1d13.webp";
+const App = () => {
