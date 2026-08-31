949127b16ffae590e3d11e45493d412db6d8bac5
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-08-31 15:47:47 +0530

    ✅ OJJAR63 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/OJJAR63/solution.cpp
@@ -0,0 +1,26 @@
+// UserProfile component: A reusable component to display user information
+function UserProfile(props) {
+  return (
+    <div>
+      <h2> User Profile:</h2>
+    </div>
+  );
+}
+
+// App component: Renders multiple UserProfile components with different data
+function App() {
+  return (
+    <div>
+      
+
+
+      
+    </div>
+  );
+}
+
+export default App; // Exporting the App component for use in the application
+      <Greeting name="John" age={25}/>
+      <Greeting name=Emma" age={28}/>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age} </p>
