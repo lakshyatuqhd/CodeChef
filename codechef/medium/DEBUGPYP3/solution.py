6fd96e61716ebe7b5a4b749a1c380b56ad90e7b6
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-02 12:41:05 +0530

    ✅ DEBUGPYP3 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/DEBUGPYP3/solution.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+if number > 0:
+    print("The number is positive")
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
