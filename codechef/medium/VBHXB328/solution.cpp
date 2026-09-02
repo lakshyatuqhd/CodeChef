04749cb0f6f1502e12b23a34c144902a55935fc4
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-02 12:31:57 +0530

    ✅ VBHXB328 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/VBHXB328/solution.cpp
@@ -0,0 +1,28 @@
+.blog-card:hover {
+    transform: translateY(-5px);
+}
+
+.blog-card h3 {
+    margin-top: 0;
+    color:  #333;
+    font-size: 1.5em;
+}
+
+.blog-content {
+    color:  #666;
+    line-height: 1.6;
+    margin-bottom: 15px;
+}
+
+.blog-author {
+    font-style: italic;
+    color:  #777;
+    font-size: 0.9em;
+    margin-bottom: 5px;
+}
+
+.blog-date {
+    font-size: 0.8em;
+    color:  #999;
+    margin-bottom: 15px;
+}
