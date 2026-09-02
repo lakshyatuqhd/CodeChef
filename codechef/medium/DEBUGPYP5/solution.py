b62710e4d12e1879f61d8f0b535b36a188d44a25
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-02 12:41:51 +0530

    ✅ DEBUGPYP5 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/DEBUGPYP5/solution.py
+++ codechef/medium/DEBUGPYP5/solution.py
@@ -1,10 +1,2 @@
-# your code goes here
-a, b, c, d = map(int, input().split())
-p = a - c # price of tv1 after discount was calculated wrong
-q = b - d # price of tv2 after discount was calculated wrong
-if p < q:
-    print("First")
-elif p > q:
-    print("Second")
-else:
-    print("Any")
+c, x, y = map(int, input().split())
+print((c - x)* y) #previous opertaions were incorrect
