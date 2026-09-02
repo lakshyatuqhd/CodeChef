ec165eb210aa8b469680d9324082710551253e70
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-02 12:41:32 +0530

    ✅ DEBUGPYP4 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/DEBUGPYP4/solution.py
+++ codechef/medium/DEBUGPYP4/solution.py
@@ -1,3 +1,10 @@
 # your code goes here
-k, x = map(int, input().split())
-print(k-x) # previous operation inside print was wrong
+a, b, c, d = map(int, input().split())
+p = a - c # price of tv1 after discount was calculated wrong
+q = b - d # price of tv2 after discount was calculated wrong
+if p < q:
+    print("First")
+elif p > q:
+    print("Second")
+else:
+    print("Any")
