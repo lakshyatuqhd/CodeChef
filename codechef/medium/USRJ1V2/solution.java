61b453840237edac0607d18490241f6c1efedd0c
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-04 20:43:49 +0530

    ✅ USRJ1V2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/USRJ1V2/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner objName = new Scanner(System.in);
+		String varName = objName.nextLine();
+		System.out.println(varName);
+	}
+}
