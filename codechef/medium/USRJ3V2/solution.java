f889055eea3c0647cbe0233e7484afcb67f9e17b
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-04 20:46:03 +0530

    ✅ USRJ3V2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/USRJ3V2/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		String x = read.nextLine();
+		System.out.println("Hello " + x);
+	}
+}
