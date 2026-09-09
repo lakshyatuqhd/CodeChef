c59aa6ae4b61c3375e2d2c29c6c1689eb6c66955
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-09 12:36:23 +0530

    ✅ VBHXB15 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/VBHXB15/solution.cpp
@@ -0,0 +1,33 @@
+    })
+      return addMilk(brewedCoffee);
+      console.log("2. Coffee brewed:", brewedCoffee);
+    .then((brewedCoffee) => {
+    })
+      return brewCoffee(groundBeans);
+      console.log("1. Beans ground:", groundBeans);
+    .then((groundBeans) => {
+  grindBeans()
+  
+  }
+    });
+      }, 500);
+        resolve("Coffee with milk: " + brewedCoffee);
+      setTimeout(() => {
+      setTimeout(() => {
+        resolve("Brewed coffee with " + groundBeans);
+      }, 500);
+    });
+  }
+  
+  function addMilk(brewedCoffee) {
+    return new Promise((resolve) => {
+  function brewCoffee(groundBeans) {
+    return new Promise((resolve) => {
+  
+  }
+    });
+      }, 500);
+        resolve("Ground coffee beans");
+      setTimeout(() => {
+    return new Promise((resolve) => {
+function grindBeans() {
