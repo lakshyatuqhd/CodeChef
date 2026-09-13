f83b23b68b7cb4e2692af10c7be27ae6f489a3bf
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-13 17:54:00 +0530

    ✅ VBHXB77 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/medium/VBHXB77/solution.cpp
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/hello', (req, res) => {
+  res.send(req.url);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
