5398ee893e6c5c82c8cc0fb2cca001781dde9f16
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-16 12:51:40 +0530

    ✅ OJJAR117 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/OJJAR117/solution.cpp
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Tabs() {
+  return (
+      <div className="tabs-container">
+            <div className="tab-headers">
+                    <span>Tab 1 Header</span>
+                          </div>
+                                <div className="tab-content">
+                                        <p>Tab Content Will Appear Here</p>
+                                              </div>
+                                                    <div className="tab-navigation">
+                                                            <button>Previous</button>
+                                                                    <button>Next</button>
+                                                                          </div>
+                                                                              </div>
+                                                                                );
+                                                                                }
+
+                                                                                export default Tabs;
