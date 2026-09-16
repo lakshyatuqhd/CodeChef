207a83b47a3be23d364dbaf776aa36692832c099
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-16 11:22:31 +0530

    ✅ FYAOAZ06 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/FYAOAZ06/solution.cpp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Greeting Page</title>
+</head>
+<body>
+  <h1>Hello, <%= user.firstName %> <%= user.lastName %>!</h1>
+</body>
+</html>
