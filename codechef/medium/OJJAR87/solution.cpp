2e1a9b6ccb454c711dd5fd627153eedccb2c136d
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-17 21:09:50 +0530

    ✅ OJJAR87 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/OJJAR87/solution.cpp
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [fruit, setFruit] = useState("banana");
+
+    return (
+        <div>
+              <select value={fruit} onChange={(e) => setFruit(e.target.value)}>
+                      <option value="apple">Apple</option>
+                              <option value="banana">Banana</option>
+                                    </select>
+
+                                          <p>
+                                                  You have selected: <strong>{fruit}</strong>.
+                                                        </p>
+                                                            </div>
+                                                              );
+                                                              }
