396363dca718f03ab34e0892823b0263f1c17125
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-20 21:51:21 +0530

    ✅ OJJAR218 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/OJJAR218/solution.cpp
@@ -0,0 +1,30 @@
+    date: '2023-01-01T12:30:00.000Z',
+    metadata: {
+      invitees: [
+        { name: 'Samantha', email: 'samboombox123@aol.com' },
+      ],
+    },
+  },
+  {
+    eventId: 'focus-time',
+    date: '2023-01-01T15:00:00.000Z',
+    metadata: { notes: 'Time for me to focus!' },
+  },
+  {
+    eventId: 'team-meeting',
+    date: '2023-01-02T10:00:00.000Z',
+    metadata: {
+      notes: 'Weekly team catch-up call!',
+      invitees: [
+        { name: 'Sadb Fabian', email: 'sfabian@widgetco.com' },
+        { name: 'Gerarda Nicomedes', email: 'gnicomedes@widgetco.com' }, // Target for removal
+  {
+    eventId: 'coffee-with-samantha',
+// Initial state data (same as before)
+const initialEventsData = [
+
+// 1. Import the CSS file
+import './App.css';
+
+import { produce } from 'immer';
+import  { useState } from 'react';
