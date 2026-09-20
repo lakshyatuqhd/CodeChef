7a106b66ffb29c2a33f6b19858f770ebff9f07fb
lakshyatuqhd <lakshyatagarapu@gmail.com>
2026-09-20 12:54:04 +0530

    ✅ OJJAR224 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/OJJAR224/solution.cpp
@@ -0,0 +1,16 @@
+import Square from './Square';
+
+function Board({ squares, onClick }) {
+    return (
+      <div className="board">
+        {squares.map((square, index) => (
+          <Square 
+            key={index}
+            value={square}
+            onClick={() => onClick(index)}
+          />
+        ))}
+      </div>
+    );
+  }
+export default Board;
